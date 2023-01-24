c91ecfd93312b99aa998fb75779623d087b800b4
shauraya <s_salwan164090@fanshaweonline.ca>
2023-01-24 18:44:39 -0500

    did some new changes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,41 @@ function logid(){
 }
 book.addEventListener('click',logid);
 
+
+let supermario = document.querySelector('#supermario');
+function logid(){
+    console.log(this.id);
+}
+supermario.addEventListener('click',logid);
+
+let jigsaw = document.querySelector('#jigsaw');
+function logid(){
+    console.log(this.id);
+}
+jigsaw.addEventListener('click',logid);
+
+
+let walter = document.querySelector('#walter');
+function logid(){
+    console.log(this.id);
+}
+walter.addEventListener('click',logid);
+
+
+let superman = document.querySelector('#superman');
+function logid(){
+    console.log(this.id);
+}
+superman.addEventListener('click',logid);
+
+
+let Apple = document.querySelector('#Apple');
+function logid(){
+    console.log(this.id);
+}
+Apple.addEventListener('click',logid);
+
+
+
+
+
